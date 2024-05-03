983e247b151489998be7b17d171dfcbdc3ef08d9
Wonbae Kim <wbkim0504@gmail.com>
2024-05-03 13:21:59 +0900

    tutorial 18.3

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,26 +1,11 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
-import { Link, Outlet } from "react-router-dom";
-import { FaAirbnb } from "react-icons/fa";
+import { Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 export default function Root() {
   return (
     <Box>
-      <HStack
-        justifyContent={"space-between"}
-        py={5}
-        px={10}
-        borderBottomWidth={1}
-      >
-        <Box color="red.500">
-          <Link to={"/"}>
-            <FaAirbnb size={"48"} />
-          </Link>
-        </Box>
-        <HStack spacing={2}>
-          <Button>Log in</Button>
-          <Button colorScheme={"red"}>Sign up</Button>
-        </HStack>
-      </HStack>
+      <Header />
     </Box>        
   );
 }
