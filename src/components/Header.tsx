b879e1df98ddb10bc1e63957b192be5e0fe8fa38
Wonbae Kim <wbkim0504@gmail.com>
2024-05-04 01:05:27 +0900

    tutorial 19.8

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import {
   HStack,
   IconButton,
   LightMode,
+  Stack,
   useColorMode,
   useColorModeValue,
   useDisclosure,
@@ -29,10 +30,19 @@ export default function Header() {
   const Icon = useColorModeValue(FaMoon, FaSun);
 
   return (
-    <HStack
+    <Stack
       justifyContent={"space-between"}
+      alignItems="center"
       py={5}
-      px={10}
+      px={40}
+      direction={{
+        sm: "column",
+        md: "row",
+      }}
+      spacing={{
+        sm: 4,
+        md: 0,
+      }}      
       borderBottomWidth={1}
     >
       <Box color={logoColor}>
@@ -56,6 +66,6 @@ export default function Header() {
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
-    </HStack>
+    </Stack>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,64 @@
+import { Grid } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getRooms } from "../api";
+
+import Room from "../components/Room";
+import RoomSkeleton from "../components/RoomSkeleton";
+
+import { IRoomList } from "../types";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <RoomSkeleton />
+//     </div>
+//   )
+// }
+
 export default function Home() {
-    return <span>homeeee</span>;
-  }
-  
+  const { isLoading, data } = useQuery<IRoomList[]>({queryKey:["rooms"], queryFn:getRooms});
+  return (
+    <Grid
+      mt={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {data?.map((room) => (
+        // <div>
+        //   {room.photos.length}
+        // </div>
+        <Room
+          key={room.pk}
+          pk={room.pk}          
+          imageUrl={room.photos[0].file}
+          name={room.name}
+          rating={room.rating}
+          city={room.city}
+          country={room.country}
+          price={room.price}
+        />
+      ))}
+    </Grid>
+  );
+}
