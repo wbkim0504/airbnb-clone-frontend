93c8cb7a16213cfcab7181a50d6a02208dc30f18
Wonbae Kim <wbkim0504@gmail.com>
2024-05-04 23:35:25 +0900

    tutorial 20.1

---
+++ src/lib/useUser.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMe } from "../api";
+import { IUser } from "../types";
+
+export default function useUser() {
+  const { isLoading, data, isError } = useQuery<IUser>({queryKey:["me"], queryFn:getMe, retry:false});
+  return {
+    userLoading: isLoading,
+    user: data,
+    isLoggedIn: !isError,
+  };
+}
+
--- src/types.d.ts
+++ src/types.d.ts
@@ -51,3 +51,16 @@ export interface IRoomPhotoPhoto {
     user: IRoomOwner;
   }
   
+  
+export interface IUser {
+  last_login: string;
+  username: string;
+  email: string;
+  date_joined: string;
+  avatar: string;
+  name: string;
+  is_host: boolean;
+  gender: string;
+  language: string;
+  currency: string;
+}
